7c17078507337892787d49558f5f948c850e512d
konwajs <konrad.wajs@capgemini.com>
2020-01-15 17:02:50 +0100

    update backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  endpoint: 'http://18.196.150.113/v1/api/'  
+  endpoint: 'http://ec2-3-126-50-177.eu-central-1.compute.amazonaws.com/v1/api/'  
 };
